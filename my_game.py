1007edd8dff748bf2c6707335959e8be2c11df01
Akimov232 <87235842+Akimov232@users.noreply.github.com>
2021-08-05 00:45:42 +0300

    Update my_game.py

--- my_game.py
+++ my_game.py
@@ -18,14 +18,54 @@ def print_filed(n):
 
 
 def start_up(n):
+    filed_size = range(len(n))
+    fide = n[:]
+    for i in filed_size:
+        for r in filed_size:
+            if n[i][r] == 'x':
+                fide[i] , fide[i - 1] = fide[i-1] , fide[i]
+                break
+    return fide 
+
+def start_down(n):
+    filed_size = range(len(n))
+    fide = n[:]
+    for i in filed_size:
+        for r in filed_size:
+            if n[i][r] == 'x':
+                if i == len(n) - 1:
+                    fide[0], fide[i - 1] = fide[i - 1] , fide[0]
+                else:
+                    fide[i] , fide[i + 1] = fide[i +1] , fide[i]
+                    break
+    return fide
 
 
+def start_right(n):
+    filed_size = range(len(n))
+    fide = n[:]
+    for i in filed_size:
+        for r in filed_size:
+            if n[i][r] == 'x':
+                fide[i][r] = '_'
+                if r == len(n) - 1:
+                    fide[i][0] = 'x'
+                else:
+                    fide[i][r+1] = 'x'
+                    break
+    return fide 
 
 
-
-
-
-
+def start_left(n):
+    filde_size = range(len(n))
+    fide = n[:]
+    for i in filde_size:
+        for r in filde_size:
+            if n[i][r] == 'x':
+                fide[i][r] = '_'
+                fide[i][r -1] = 'x'
+                break
+    return fide 
 
 
 def user_guess_direction():
@@ -38,7 +78,6 @@ def user_input_size():
 
 
 
-
 def main():
     game = True
     size = user_input_size()
@@ -47,6 +86,17 @@ def main():
     while game:
         user_direction = user_guess_direction()
 
-
         if user_direction == 'up':
             make_filed = start_up(make_filed)
+        if user_direction == 'down':
+            make_filed = start_down(make_filed)
+        if user_direction == 'right':
+            make_filed = start_right(make_filed)
+        if user_direction == 'left':
+            make_filed = start_left(make_filed)
+        if user_direction == 'exit':
+            game = False 
+        if game:
+            print_filed(make_filed)
+
+main()
